969c0cb8275b48b54fba521684e89fdf202e8396
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2023-01-29 23:29:14 +0100

    Update setup.py

--- ros2_kitti_description/setup.py
+++ ros2_kitti_description/setup.py
@@ -13,7 +13,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.py')),
-        (os.path.join('share', package_name), glob('urdf/*'))
+        (os.path.join('share', package_name), glob('urdf/*')),
+        (os.path.join('share', package_name), glob('rviz/*'))
     ],
     entry_points={
         'console_scripts': [
