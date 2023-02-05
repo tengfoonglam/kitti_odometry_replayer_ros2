7b01b087b52888b2f01eca7d54f3501d8624706b
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2023-02-05 18:10:52 +0100

    doxygen

--- ros2_kitti_replay/include/ros2_kitti_replay/ordered_timestamps.hpp
+++ ros2_kitti_replay/include/ros2_kitti_replay/ordered_timestamps.hpp
@@ -11,6 +11,17 @@ namespace r2k_replay
 
 using OrderedTimestamps = std::set<rclcpp::Time>;
 
+/**
+ * @brief      Extract timestamps from a KITTI times .txt file
+ * @note       It is the user's responsibility to ensure that
+ *             1) Each line in the file corresponds to a timestamp
+ *             2) Timestamps are in increasing order (or else iterating over OrderedTimestamps will not give the same sequence as in the file)
+ *             3) Timestamps are positive
+ *
+ * @param[in]  times_path  Path to the times file
+ *
+ * @return     { description_of_the_return_value }
+ */
 [[nodiscard]] std::optional<OrderedTimestamps> extract_ordered_timestamps_from_file(
   const std::filesystem::path & times_path);
 
