b6cd0fcc218dea794872b1452bcd34475dd4d020
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2023-05-19 19:44:17 +0200

    Update gt path when setting time ragne

--- ros2_kitti_replay/src/kitti_replayer_node.cpp
+++ ros2_kitti_replay/src/kitti_replayer_node.cpp
@@ -51,7 +51,7 @@ KITTIReplayerNode::KITTIReplayerNode(const rclcpp::NodeOptions & options)
   const auto point_cloud_folder_path = std::filesystem::path(
     parameters_client.get_parameter("point_cloud_folder_path", std::string{""}));
 
-  // Load ground truth pose (if available)
+  // Load ground truth pose for visualization (if available)
   ground_truth_path_opt_ = extract_poses_from_file(poses_path);
   if (ground_truth_path_opt_.has_value()) {
     RCLCPP_INFO(get_logger(), "Ground truth path loaded for this dataset");
@@ -90,21 +90,23 @@ KITTIReplayerNode::KITTIReplayerNode(const rclcpp::NodeOptions & options)
   play_data_interface_ptrs.push_back(clock_interface_ptr);
 
   // Ground Truth Pose (if available)
-  r2k_replay::PoseDataLoader::Header pose_header;
-  pose_header.frame_id = "map";
-  const std::string child_id{"p0"};
-  auto pose_loader_ptr = std::make_unique<PoseDataLoader>(
-    "pose_loader", get_logger().get_child("pose_loader"), pose_header, child_id);
-  pose_loader_ptr->setup(timestamps, poses_path);
-  auto pose_interface_ptr = make_shared_interface(
-    "pose_interface",
-    [broadcast = tf2_ros::TransformBroadcaster(*this)](const auto & msg) mutable {
-      broadcast.sendTransform(msg);
-      return true;
-    },
-    std::move(pose_loader_ptr));
-  play_data_interface_check_shutdown_if_fail(*pose_interface_ptr, number_stamps);
-  play_data_interface_ptrs.push_back(pose_interface_ptr);
+  if (ground_truth_path_opt_.has_value()) {
+    r2k_replay::PoseDataLoader::Header pose_header;
+    pose_header.frame_id = "map";
+    const std::string child_id{"p0"};
+    auto pose_loader_ptr = std::make_unique<PoseDataLoader>(
+      "pose_loader", get_logger().get_child("pose_loader"), pose_header, child_id);
+    pose_loader_ptr->setup(timestamps, poses_path);
+    auto pose_interface_ptr = make_shared_interface(
+      "pose_interface",
+      [broadcast = tf2_ros::TransformBroadcaster(*this)](const auto & msg) mutable {
+        broadcast.sendTransform(msg);
+        return true;
+      },
+      std::move(pose_loader_ptr));
+    play_data_interface_check_shutdown_if_fail(*pose_interface_ptr, number_stamps);
+    play_data_interface_ptrs.push_back(pose_interface_ptr);
+  }
 
   // Point Cloud
   r2k_replay::PointCloudDataLoader::Header pc_header;
@@ -194,9 +196,22 @@ void KITTIReplayerNode::set_time_range(
   const std::shared_ptr<SetTimeRangeSrv::Request> request_ptr,
   std::shared_ptr<SetTimeRangeSrv::Response> response_ptr)
 {
-  response_ptr->response.success = replayer_ptr_->set_time_range(
+  const auto success = replayer_ptr_->set_time_range(
     {Timestamp(request_ptr->request.start_time, RCL_SYSTEM_TIME),
      Timestamp(request_ptr->request.end_time, RCL_SYSTEM_TIME)});
+  response_ptr->response.success = success;
+
+  if (success && ground_truth_path_opt_.has_value()) {
+    const auto & ground_truth_path = ground_truth_path_opt_.value();
+    const auto replayer_state = replayer_ptr_->get_replayer_state();
+    const auto start_it = std::next(std::cbegin(ground_truth_path), replayer_state.next_idx);
+    const auto end_idx = (replayer_state.target_idx + 1 >= replayer_state.data_size)
+                           ? replayer_state.data_size
+                           : replayer_state.target_idx + 1;
+    const auto end_it = std::next(std::cbegin(ground_truth_path), end_idx);
+    publish_ground_truth_path({start_it, end_it});
+    RCLCPP_INFO(get_logger(), "Updated ground truth path");
+  }
 }
 
 template <typename T>
