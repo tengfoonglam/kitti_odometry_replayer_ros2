68d18bdc31def49c77a4ceecf90e36ab07deaed6
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2023-07-26 21:59:45 +0200

    Add image data loader

---
+++ ros2_kitti_core/include/ros2_kitti_core/image_data_loader.hpp
@@ -0,0 +1,32 @@
+#ifndef ROS2_KITTI_CORE__IMAGE_DATA_LOADER_HPP_
+#define ROS2_KITTI_CORE__IMAGE_DATA_LOADER_HPP_
+
+#include <optional>
+#include <std_msgs/msg/header.hpp>
+#include <string>
+
+#include "ros2_kitti_core/folder_data_loader.hpp"
+#include "ros2_kitti_core/image_utils.hpp"
+#include "ros2_kitti_core/timestamp_utils.hpp"
+
+namespace r2k_core
+{
+
+class ImageDataLoader final : public FolderDataLoader<ImageMsg::SharedPtr>
+{
+public:
+  using Header = std_msgs::msg::Header;
+
+  ImageDataLoader(const std::string & name, const Header & header);
+  ImageDataLoader(const std::string & name, rclcpp::Logger logger, const Header & header);
+
+private:
+  [[nodiscard]] std::optional<size_t> get_last_index_of_sequence(
+    const std::filesystem::path & load_path) final;
+
+  [[nodiscard]] ImageDataLoader::ReturnType load_data(
+    const std::size_t idx, const std::filesystem::path & load_path) final;
+};
+
+}  // namespace r2k_core
+#endif  // ROS2_KITTI_CORE__IMAGE_DATA_LOADER_HPP_
---
+++ ros2_kitti_core/src/image_data_loader.cpp
@@ -0,0 +1,29 @@
+#include "ros2_kitti_core/image_data_loader.hpp"
+
+namespace r2k_core
+{
+
+ImageDataLoader::ImageDataLoader(const std::string & name, const Header & header)
+: FolderDataLoader<ImageDataLoader::ReturnType>(name, header)
+{
+}
+
+ImageDataLoader::ImageDataLoader(
+  const std::string & name, rclcpp::Logger logger, const Header & header)
+: FolderDataLoader<ImageDataLoader::ReturnType>(name, logger, header)
+{
+}
+
+[[nodiscard]] std::optional<size_t> ImageDataLoader::get_last_index_of_sequence(
+  const std::filesystem::path & load_path)
+{
+  return get_last_index_of_image_sequence(load_path);
+}
+
+[[nodiscard]] ImageDataLoader::ReturnType ImageDataLoader::load_data(
+  const std::size_t idx, const std::filesystem::path & load_path)
+{
+  return load_image_from_file(from_index_to_image_file_path(idx, load_path));
+}
+
+}  // namespace r2k_core
