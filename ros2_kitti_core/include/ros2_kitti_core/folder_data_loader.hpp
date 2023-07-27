53a38bf294f4e3fad876ebde411701d45a392d66
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2023-07-27 21:47:26 +0200

    move to source file

--- ros2_kitti_core/include/ros2_kitti_core/folder_data_loader.hpp
+++ ros2_kitti_core/include/ros2_kitti_core/folder_data_loader.hpp
@@ -7,7 +7,6 @@
 #include <string>
 
 #include "ros2_kitti_core/data_loader.hpp"
-#include "ros2_kitti_core/point_cloud_utils.hpp"
 #include "ros2_kitti_core/timestamp_utils.hpp"
 #include "ros2_kitti_core/type_checks.hpp"
 
@@ -24,24 +23,15 @@ class FolderDataLoader : public DataLoader<RT>
 public:
   using Header = std_msgs::msg::Header;
 
-  FolderDataLoader(const std::string & name, const Header & header)
-  : DataLoader<RT>(name), header_(header)
-  {
-  }
+  FolderDataLoader(const std::string & name, const Header & header);
 
-  FolderDataLoader(const std::string & name, rclcpp::Logger logger, const Header & header)
-  : DataLoader<RT>(name, logger), header_(header)
-  {
-  }
+  FolderDataLoader(const std::string & name, rclcpp::Logger logger, const Header & header);
 
-  [[nodiscard]] std::size_t data_size() const override
-  {
-    return this->ready() ? max_idx_ + 1 : std::size_t{0};
-  }
+  [[nodiscard]] std::size_t data_size() const final;
 
 protected:
   Header header_;
-  std::size_t max_idx_{};
+  std::size_t max_idx_{0};
   RT data_ptr_;
   std::optional<size_t> current_idx_opt_;
   std::filesystem::path load_path_;
@@ -52,54 +42,11 @@ protected:
   [[nodiscard]] virtual RT load_data(
     const std::size_t idx, const std::filesystem::path & load_path) = 0;
 
-  bool setup_internal(const Timestamps & timestamps, const std::filesystem::path & load_path) final
-  {
-    const auto data_idx_opt = get_last_index_of_sequence(load_path);
+  bool setup_internal(const Timestamps & timestamps, const std::filesystem::path & load_path) final;
 
-    if (data_idx_opt.has_value() && !timestamps.empty()) {
-      this->timestamps_ = timestamps;
-      load_path_ = load_path;
-      max_idx_ = std::min(data_idx_opt.value(), this->timestamps_.size() - 1);
-      this->ready_ = true;
-      RCLCPP_INFO(
-        this->logger_, "%s point cloud data loader setup successful. Number of point clouds: %zu",
-        this->name().c_str(), max_idx_ + 1);
-    } else {
-      if (timestamps.empty()) {
-        RCLCPP_WARN(
-          this->logger_, "%s point cloud data loader setup failed. No timestamps provided",
-          this->name().c_str());
-      } else {
-        RCLCPP_WARN(
-          this->logger_,
-          "%s point cloud data loader setup failed. Could not find point cloud files at %s",
-          this->name().c_str(), load_path.string().c_str());
-      }
-    }
+  bool prepare_data_internal(const std::size_t idx) final;
 
-    return this->ready();
-  };
-
-  bool prepare_data_internal(const std::size_t idx) final
-  {
-    data_ptr_ = this->load_data(idx, load_path_);
-
-    if (data_ptr_) {
-      data_ptr_->header = header_;
-      data_ptr_->header.stamp = this->timestamps_.at(idx);
-      current_idx_opt_ = idx;
-    }
-
-    return static_cast<bool>(data_ptr_);
-  };
-
-  [[nodiscard]] std::optional<RT> get_data_internal(const std::size_t idx) final
-  {
-    if (current_idx_opt_.has_value() && current_idx_opt_.value() == idx && data_ptr_) {
-      return data_ptr_;
-    }
-    return std::nullopt;
-  };
+  std::optional<RT> get_data_internal(const std::size_t idx) final;
 };
 
 }  // namespace r2k_core
---
+++ ros2_kitti_core/src/folder_data_loader.cpp
@@ -0,0 +1,85 @@
+#include "ros2_kitti_core/folder_data_loader.hpp"
+
+#include "ros2_kitti_core/image_utils.hpp"
+#include "ros2_kitti_core/point_cloud_utils.hpp"
+
+namespace r2k_core
+{
+
+template <typename RT>
+FolderDataLoader<RT>::FolderDataLoader(const std::string & name, const Header & header)
+: DataLoader<RT>(name), header_(header)
+{
+}
+
+template <typename RT>
+FolderDataLoader<RT>::FolderDataLoader(
+  const std::string & name, rclcpp::Logger logger, const Header & header)
+: DataLoader<RT>(name, logger), header_(header)
+{
+}
+
+template <typename RT>
+std::size_t FolderDataLoader<RT>::data_size() const
+{
+  return this->ready() ? max_idx_ + 1 : std::size_t{0};
+}
+
+template <typename RT>
+bool FolderDataLoader<RT>::setup_internal(
+  const Timestamps & timestamps, const std::filesystem::path & load_path)
+{
+  const auto data_idx_opt = get_last_index_of_sequence(load_path);
+
+  if (data_idx_opt.has_value() && !timestamps.empty()) {
+    this->timestamps_ = timestamps;
+    load_path_ = load_path;
+    max_idx_ = std::min(data_idx_opt.value(), this->timestamps_.size() - 1);
+    this->ready_ = true;
+    RCLCPP_INFO(
+      this->logger_, "%s point cloud data loader setup successful. Number of point clouds: %zu",
+      this->name().c_str(), max_idx_ + 1);
+  } else {
+    if (timestamps.empty()) {
+      RCLCPP_WARN(
+        this->logger_, "%s point cloud data loader setup failed. No timestamps provided",
+        this->name().c_str());
+    } else {
+      RCLCPP_WARN(
+        this->logger_,
+        "%s point cloud data loader setup failed. Could not find point cloud files at %s",
+        this->name().c_str(), load_path.string().c_str());
+    }
+  }
+
+  return this->ready();
+};
+
+template <typename RT>
+bool FolderDataLoader<RT>::prepare_data_internal(const std::size_t idx)
+{
+  data_ptr_ = this->load_data(idx, load_path_);
+
+  if (data_ptr_) {
+    data_ptr_->header = header_;
+    data_ptr_->header.stamp = this->timestamps_.at(idx);
+    current_idx_opt_ = idx;
+  }
+
+  return static_cast<bool>(data_ptr_);
+};
+
+template <typename RT>
+std::optional<RT> FolderDataLoader<RT>::get_data_internal(const std::size_t idx)
+{
+  if (current_idx_opt_.has_value() && current_idx_opt_.value() == idx && data_ptr_) {
+    return data_ptr_;
+  }
+  return std::nullopt;
+};
+
+// Explicit template instantiation so we can keep methods in source file
+template class FolderDataLoader<PointCloudMsg::SharedPtr>;
+template class FolderDataLoader<ImageMsg::SharedPtr>;
+
+}  // namespace r2k_core
