84d4acc24031e33dae9abe3f315b1084aa91d7e6
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2023-07-30 13:18:25 +0200

    Fix ground truth replaying

--- ros2_kitti_replay/launch/replayer_bringup.launch.py
+++ ros2_kitti_replay/launch/replayer_bringup.launch.py
@@ -19,9 +19,10 @@ from launch.substitutions import (
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
+from typing import List
 
 
-def launch_setup(context: LaunchContext) -> LaunchDescription:
+def launch_setup(context: LaunchContext) -> List[LaunchDescription]:
     # Replayer Launch Configurations
     use_sim_time = LaunchConfiguration("use_sim_time", default="true")
 
@@ -67,19 +68,20 @@ def launch_setup(context: LaunchContext) -> LaunchDescription:
     # specify your own frame_id prefix
     OVERRIDE_ODOMETRY_DATA_PREFIX = True
 
-    processed_odometry_data_frame_prefix = PythonExpression(
-        [
-            '"',
-            ground_truth_data_frame_prefix,
-            '" if ((not',
-            launch_odometry,
-            ") and ",
-            OVERRIDE_ODOMETRY_DATA_PREFIX,
-            ') else "',
-            odometry_data_frame_prefix,
-            '"',
-        ]
-    )
+    if OVERRIDE_ODOMETRY_DATA_PREFIX:
+        processed_odometry_data_frame_prefix = PythonExpression(
+            [
+                '"',
+                odometry_data_frame_prefix,
+                '" if ',
+                launch_odometry,
+                ' else "',
+                ground_truth_data_frame_prefix,
+                '"',
+            ]
+        )
+    else:
+        processed_odometry_data_frame_prefix = odometry_data_frame_prefix
 
     # Paths of data files
     dataset_number_padded = PythonExpression(['f"{', dataset_number, ':02}"'])
@@ -175,8 +177,8 @@ def launch_setup(context: LaunchContext) -> LaunchDescription:
                 "point_cloud_folder_path": point_cloud_folder_path,
                 "gray_image_folder_path": gray_image_folder_path,
                 "colour_image_folder_path": colour_image_folder_path,
-                "ground_truth_data_frame_prefix": processed_odometry_data_frame_prefix,
-                "odometry_data_frame_prefix": odometry_data_frame_prefix,
+                "ground_truth_data_frame_prefix": ground_truth_data_frame_prefix,
+                "odometry_data_frame_prefix": processed_odometry_data_frame_prefix,
                 "odometry_reference_frame_id": odometry_reference_frame_id,
             }
         ],
@@ -198,126 +200,126 @@ def launch_setup(context: LaunchContext) -> LaunchDescription:
         ],
     )
 
-    return LaunchDescription(
-        [
-            IncludeLaunchDescription(
-                launch_description_source=PythonLaunchDescriptionSource(
-                    [
-                        get_package_share_directory("ros2_kitti_description"),
-                        "/load_urdf.launch.py",
-                    ]
+    return [
+        IncludeLaunchDescription(
+            launch_description_source=PythonLaunchDescriptionSource(
+                [
+                    get_package_share_directory("ros2_kitti_description"),
+                    "/load_urdf.launch.py",
+                ]
+            ),
+            launch_arguments={
+                "use_sim_time": use_sim_time,
+                "urdf_filename": urdf_filename,
+                "launch_rviz": "False",
+                "frame_prefix": ground_truth_data_frame_prefix,
+            }.items(),
+        ),
+        IncludeLaunchDescription(
+            launch_description_source=PythonLaunchDescriptionSource(
+                [
+                    get_package_share_directory("ros2_kitti_description"),
+                    "/load_urdf.launch.py",
+                ]
+            ),
+            launch_arguments={
+                "use_sim_time": use_sim_time,
+                "urdf_filename": urdf_filename,
+                "launch_rviz": "False",
+                "frame_prefix": odometry_data_frame_prefix,
+                "static_frame_base_frame_id": "odom",
+            }.items(),
+            condition=IfCondition(launch_odometry),
+        ),
+        ComposableNodeContainer(
+            name="replayer_container",
+            namespace="",
+            package="rclcpp_components",
+            executable="component_container_mt",
+            composable_node_descriptions=[replayer_component],
+            output="screen",
+            condition=IfCondition(PythonExpression(["not ", launch_odometry])),
+        ),
+        ComposableNodeContainer(
+            name="replayer_with_odometry_container",
+            namespace="",
+            package="rclcpp_components",
+            executable="component_container_mt",
+            composable_node_descriptions=[replayer_component, odometry_component],
+            output="screen",
+            condition=IfCondition(launch_odometry),
+        ),
+        TimerAction(
+            period=5.0,
+            actions=[
+                Node(
+                    package="rviz2",
+                    namespace="",
+                    executable="rviz2",
+                    name="rviz2",
+                    parameters=[{"use_sim_time": use_sim_time}],
+                    arguments=[rviz_path_arg],
                 ),
-                launch_arguments={
-                    "use_sim_time": use_sim_time,
-                    "urdf_filename": urdf_filename,
-                    "launch_rviz": "False",
-                    "frame_prefix": ground_truth_data_frame_prefix,
-                }.items(),
-            ),
-            IncludeLaunchDescription(
-                launch_description_source=PythonLaunchDescriptionSource(
-                    [
-                        get_package_share_directory("ros2_kitti_description"),
-                        "/load_urdf.launch.py",
-                    ]
-                ),
-                launch_arguments={
-                    "use_sim_time": use_sim_time,
-                    "urdf_filename": urdf_filename,
-                    "launch_rviz": "False",
-                    "frame_prefix": odometry_data_frame_prefix,
-                    "static_frame_base_frame_id": "odom",
-                }.items(),
-                condition=IfCondition(launch_odometry),
-            ),
-            ComposableNodeContainer(
-                name="replayer_container",
-                namespace="",
-                package="rclcpp_components",
-                executable="component_container_mt",
-                composable_node_descriptions=[replayer_component],
-                output="screen",
-                condition=IfCondition(PythonExpression(["not ", launch_odometry])),
-            ),
-            ComposableNodeContainer(
-                name="replayer_with_odometry_container",
-                namespace="",
-                package="rclcpp_components",
-                executable="component_container_mt",
-                composable_node_descriptions=[replayer_component, odometry_component],
-                output="screen",
-                condition=IfCondition(launch_odometry),
-            ),
-            TimerAction(
-                period=5.0,
-                actions=[
-                    Node(
-                        package="rviz2",
-                        namespace="",
-                        executable="rviz2",
-                        name="rviz2",
-                        parameters=[{"use_sim_time": use_sim_time}],
-                        arguments=[rviz_path_arg],
-                    ),
-                ],
-            ),
-        ]
-    )
+            ],
+        ),
+    ]
 
 
 def generate_launch_description() -> LaunchDescription:
-    return [
-        DeclareLaunchArgument(
-            "dataset_number",
-            default_value="00",
-            description="Dataset number to load. Omit any leading zeros.",
-        ),
-        DeclareLaunchArgument(
-            "dataset_path",
-            default_value="/media/ltf/LTFUbuntuSSD/kitti_dataset",
-            description="Path where all dataset folders are located",
-        ),
-        DeclareLaunchArgument(
-            "ground_truth_data_frame_prefix",
-            default_value="ground_truth",
-            description="Prefix prepended to the frame_id of the ground truth pose message",
-        ),
-        DeclareLaunchArgument(
-            "odometry_data_frame_prefix",
-            default_value="odometry",
-            description="Prefix prepended to the frame_id of the published data topics such as"
-            "point clouds, images, etc. "
-            "Note: Only used then a odometry node is specified to be launched. Otherwise, "
-            "the odometry data topics will be prepended with the ground_truth_data_frame_prefix "
-            "instead. This is so that the data topics are visualized correctly "
-            "on the ground truth vehicle when an odometry node is not running. "
-            "If you want to disable this automatic overriding of the frame prefix, set the "
-            "variable OVERRIDE_ODOMETRY_DATA_PREFIX in the launch file to False",
-        ),
-        DeclareLaunchArgument(
-            "odometry_package",
-            default_value="",
-            description="Package in which odometry component is located. "
-            "Leave empty if launching odometry component is not required",
-        ),
-        DeclareLaunchArgument(
-            "odometry_plugin",
-            default_value="",
-            description="Name of odometry component to launch. "
-            "Leave empty if launching odometry component is not required",
-        ),
-        DeclareLaunchArgument(
-            "vehicle_sensor_link",
-            default_value="lidar",
-            description="Name of the link which the odometry sensor is "
-            "located (e.g. lidar, p0)"
-            "Leave empty if launching odometry component is not required",
-        ),
-        DeclareLaunchArgument(
-            "odometry_config_path",
-            default_value="",
-            description="Path config file used for the odometry component. "
-            "Leave empty if launching odometry component is not required",
-        ),
-        OpaqueFunction(function=launch_setup),
-    ]
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "dataset_number",
+                default_value="00",
+                description="Dataset number to load. Omit any leading zeros.",
+            ),
+            DeclareLaunchArgument(
+                "dataset_path",
+                default_value="/media/ltf/LTFUbuntuSSD/kitti_dataset",
+                description="Path where all dataset folders are located",
+            ),
+            DeclareLaunchArgument(
+                "ground_truth_data_frame_prefix",
+                default_value="ground_truth",
+                description="Prefix prepended to the frame_id of the ground truth pose message",
+            ),
+            DeclareLaunchArgument(
+                "odometry_data_frame_prefix",
+                default_value="odometry",
+                description="Prefix prepended to the frame_id of the published data topics such as"
+                "point clouds, images, etc. "
+                "Note: Only used then a odometry node is specified to be launched. Otherwise, the "
+                "odometry data topics will be prepended with the ground_truth_data_frame_prefix "
+                "instead. This is so that the data topics are visualized correctly "
+                "on the ground truth vehicle when an odometry node is not running. "
+                "If you want to disable this automatic overriding of the frame prefix, set the "
+                "variable OVERRIDE_ODOMETRY_DATA_PREFIX in the launch file to False",
+            ),
+            DeclareLaunchArgument(
+                "odometry_package",
+                default_value="",
+                description="Package in which odometry component is located. "
+                "Leave empty if launching odometry component is not required",
+            ),
+            DeclareLaunchArgument(
+                "odometry_plugin",
+                default_value="",
+                description="Name of odometry component to launch. "
+                "Leave empty if launching odometry component is not required",
+            ),
+            DeclareLaunchArgument(
+                "vehicle_sensor_link",
+                default_value="lidar",
+                description="Name of the link which the odometry sensor is "
+                "located (e.g. lidar, p0)"
+                "Leave empty if launching odometry component is not required",
+            ),
+            DeclareLaunchArgument(
+                "odometry_config_path",
+                default_value="",
+                description="Path config file used for the odometry component. "
+                "Leave empty if launching odometry component is not required",
+            ),
+            OpaqueFunction(function=launch_setup),
+        ]
+    )
