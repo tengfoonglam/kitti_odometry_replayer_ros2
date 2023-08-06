a637ca4ade0956ccb92325a350f0ec14b9f44cd0
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2023-08-06 18:19:11 +0200

    Skeleton template

--- ros2_kitti_rviz_plugin/src/replay_panel.cpp
+++ ros2_kitti_rviz_plugin/src/replay_panel.cpp
@@ -1,28 +1,65 @@
 #include "ros2_kitti_rviz_plugin/replay_panel.hpp"
 
+#include <QDoubleSpinBox>
+#include <QGridLayout>
 #include <QLabel>
-#include <QLineEdit>
-#include <QRadioButton>
-#include <QVBoxLayout>
-#include <rviz_common/interaction/selection_manager.hpp>
-#include <rviz_common/properties/property_tree_widget.hpp>
-#include <rviz_common/visualization_manager.hpp>
+#include <QPushButton>
+#include <QSpinBox>
 
 namespace r2k_rviz
 {
 
 ReplayPanel::ReplayPanel(QWidget * parent) : rviz_common::Panel(parent)
 {
-  auto layout = new QVBoxLayout();
+  auto * layout = new QGridLayout();
   layout->setContentsMargins(0, 0, 0, 0);
 
-  auto text = new QLabel("This is a demo");
-  auto lineEdit = new QLineEdit();
-  auto radio = new QRadioButton();
+  auto * start_time_label_ptr = new QLabel("Start Time");
+  auto * start_time_value_ptr = new QLabel("0.0");
 
-  layout->addWidget(text);
-  layout->addWidget(lineEdit);
-  layout->addWidget(radio);
+  auto * current_time_label_ptr = new QLabel("Current Time");
+  auto * current_time_value_ptr = new QLabel("0.0");
+
+  auto * end_time_label_ptr = new QLabel("End Time");
+  auto * end_time_value_ptr = new QLabel("0.0");
+
+  auto * replay_speed_label_ptr = new QLabel("Replay Speed");
+
+  auto * replay_speed_input_ptr = new QDoubleSpinBox();
+  replay_speed_input_ptr->setDecimals(2);
+  replay_speed_input_ptr->setSingleStep(0.25);
+  replay_speed_input_ptr->setRange(0.0, 5.0);
+  replay_speed_input_ptr->setValue(1.0);
+
+  auto * step_size_label_ptr = new QLabel("Step Size");
+
+  auto * step_size_input_ptr = new QSpinBox();
+  step_size_input_ptr->setSingleStep(1);
+  step_size_input_ptr->setMinimum(1);
+
+  auto * play_button_ptr = new QPushButton("Play");
+  auto * pause_button_ptr = new QPushButton("Pause");
+  auto * step_button_ptr = new QPushButton("Step");
+
+  layout->addWidget(start_time_label_ptr, 0, 0);
+  layout->addWidget(start_time_value_ptr, 0, 1);
+
+  layout->addWidget(current_time_label_ptr, 1, 0);
+  layout->addWidget(current_time_value_ptr, 1, 1);
+
+  layout->addWidget(end_time_label_ptr, 2, 0);
+  layout->addWidget(end_time_value_ptr, 2, 1);
+
+  layout->addWidget(replay_speed_label_ptr, 3, 0);
+  layout->addWidget(replay_speed_input_ptr, 3, 1);
+
+  layout->addWidget(step_size_label_ptr, 4, 0);
+  layout->addWidget(step_size_input_ptr, 4, 1);
+
+  layout->addWidget(play_button_ptr, 5, 0);
+  layout->addWidget(step_button_ptr, 5, 1);
+
+  layout->addWidget(pause_button_ptr, 6, 0, 1, 2);
 
   setLayout(layout);
 }
