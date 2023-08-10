63e1e49c2db15dc7ff904c3c2fb59f891073db21
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2023-08-10 23:42:05 +0200

    Refactor and update time displays

--- ros2_kitti_rviz_plugin/include/ros2_kitti_rviz_plugin/replay_panel.hpp
+++ ros2_kitti_rviz_plugin/include/ros2_kitti_rviz_plugin/replay_panel.hpp
@@ -1,7 +1,12 @@
 #ifndef ROS2_KITTI_RVIZ_PLUGIN__REPLAY_PANEL_HPP_
 #define ROS2_KITTI_RVIZ_PLUGIN__REPLAY_PANEL_HPP_
 
+#include <QDoubleSpinBox>
+#include <QGridLayout>
+#include <QLabel>
 #include <QMainWindow>
+#include <QPushButton>
+#include <QSpinBox>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <ros2_kitti_msgs/msg/replayer_state.hpp>
@@ -17,12 +22,12 @@ class ReplayPanel : public rviz_common::Panel  // QMainWindow
 
 public:
   static constexpr const char kReplayerStateTopicName[]{"replayer_state"};
+  static constexpr std::size_t kValueDisplayPrecision{3};
+  static constexpr char kValueDisplayFormat{'f'};
 
   using ReplayerStateMsg = ros2_kitti_msgs::msg::ReplayerState;
 
   explicit ReplayPanel(QWidget * parent = nullptr);
-
-  void onInitialize() final;
 
   ~ReplayPanel();
 
@@ -31,8 +36,21 @@ protected:
   rclcpp::Subscription<ReplayerStateMsg>::SharedPtr state_subscriber_ptr_;
   std::thread executor_thread_;
   rclcpp::executors::SingleThreadedExecutor executor_;
+  QLabel * const start_time_label_ptr_;
+  QLabel * const start_time_value_ptr_;
+  QLabel * const current_time_label_ptr_;
+  QLabel * const current_time_value_ptr_;
+  QLabel * const end_time_label_ptr_;
+  QLabel * const end_time_value_ptr_;
+  QLabel * const replay_speed_label_ptr_;
+  QDoubleSpinBox * const replay_speed_input_ptr_;
+  QLabel * const step_size_label_ptr_;
+  QSpinBox * const step_size_input_ptr_;
+  QPushButton * const play_button_ptr_;
+  QPushButton * const pause_button_ptr_;
+  QPushButton * const step_button_ptr_;
 
-  void state_callback(const ReplayerStateMsg::ConstSharedPtr state);
+  void state_callback(const ReplayerStateMsg::ConstSharedPtr state_ptr);
 };
 
 }  // namespace r2k_rviz
--- ros2_kitti_rviz_plugin/src/replay_panel.cpp
+++ ros2_kitti_rviz_plugin/src/replay_panel.cpp
@@ -1,71 +1,55 @@
 #include "ros2_kitti_rviz_plugin/replay_panel.hpp"
 
-#include <QDoubleSpinBox>
-#include <QGridLayout>
-#include <QLabel>
-#include <QPushButton>
-#include <QSpinBox>
+#include <QString>
 #include <functional>
+#include <rclcpp/time.hpp>
 
 namespace r2k_rviz
 {
 
-ReplayPanel::ReplayPanel(QWidget * parent) : rviz_common::Panel(parent)
+ReplayPanel::ReplayPanel(QWidget * parent)
+: rviz_common::Panel(parent),
+  start_time_label_ptr_(new QLabel("Start Time [s]")),
+  start_time_value_ptr_(new QLabel("0.0")),
+  current_time_label_ptr_(new QLabel("Current Time [s]")),
+  current_time_value_ptr_(new QLabel("0.0")),
+  end_time_label_ptr_(new QLabel("End Time [s]")),
+  end_time_value_ptr_(new QLabel("0.0")),
+  replay_speed_label_ptr_(new QLabel("Replay Speed")),
+  replay_speed_input_ptr_(new QDoubleSpinBox()),
+  step_size_label_ptr_(new QLabel("Step Size")),
+  step_size_input_ptr_(new QSpinBox()),
+  play_button_ptr_(new QPushButton("Play")),
+  pause_button_ptr_(new QPushButton("Pause")),
+  step_button_ptr_(new QPushButton("Step"))
 {
   auto * const layout = new QGridLayout();
   layout->setContentsMargins(0, 0, 0, 0);
 
-  auto * const start_time_label_ptr = new QLabel("Start Time");
-  auto * const start_time_value_ptr = new QLabel("0.0");
+  replay_speed_input_ptr_->setDecimals(2);
+  replay_speed_input_ptr_->setSingleStep(0.25);
+  replay_speed_input_ptr_->setRange(0.0, 5.0);
+  replay_speed_input_ptr_->setValue(1.0);
+  replay_speed_input_ptr_->setPrefix("x");
+  step_size_input_ptr_->setSingleStep(1);
+  step_size_input_ptr_->setMinimum(1);
+  play_button_ptr_->setDisabled(true);
+  pause_button_ptr_->setDisabled(true);
+  step_button_ptr_->setDisabled(true);
 
-  auto * const current_time_label_ptr = new QLabel("Current Time");
-  auto * const current_time_value_ptr = new QLabel("0.0");
-
-  auto * const end_time_label_ptr = new QLabel("End Time");
-  auto * const end_time_value_ptr = new QLabel("0.0");
-
-  auto * const replay_speed_label_ptr = new QLabel("Replay Speed");
-
-  auto * const replay_speed_input_ptr = new QDoubleSpinBox();
-  replay_speed_input_ptr->setDecimals(2);
-  replay_speed_input_ptr->setSingleStep(0.25);
-  replay_speed_input_ptr->setRange(0.0, 5.0);
-  replay_speed_input_ptr->setValue(1.0);
-
-  auto * const step_size_label_ptr = new QLabel("Step Size");
-
-  auto * const step_size_input_ptr = new QSpinBox();
-  step_size_input_ptr->setSingleStep(1);
-  step_size_input_ptr->setMinimum(1);
-
-  auto * const play_button_ptr = new QPushButton("Play");
-  play_button_ptr->setDisabled(true);
-
-  auto * const pause_button_ptr = new QPushButton("Pause");
-  pause_button_ptr->setDisabled(true);
-
-  auto * const step_button_ptr = new QPushButton("Step");
-  step_button_ptr->setDisabled(true);
-
-  layout->addWidget(start_time_label_ptr, 0, 0);
-  layout->addWidget(start_time_value_ptr, 0, 1);
-
-  layout->addWidget(current_time_label_ptr, 1, 0);
-  layout->addWidget(current_time_value_ptr, 1, 1);
-
-  layout->addWidget(end_time_label_ptr, 2, 0);
-  layout->addWidget(end_time_value_ptr, 2, 1);
-
-  layout->addWidget(replay_speed_label_ptr, 3, 0);
-  layout->addWidget(replay_speed_input_ptr, 3, 1);
-
-  layout->addWidget(step_size_label_ptr, 4, 0);
-  layout->addWidget(step_size_input_ptr, 4, 1);
-
-  layout->addWidget(play_button_ptr, 5, 0);
-  layout->addWidget(step_button_ptr, 5, 1);
-
-  layout->addWidget(pause_button_ptr, 6, 0, 1, 2);
+  layout->addWidget(start_time_label_ptr_, 0, 0);
+  layout->addWidget(start_time_value_ptr_, 0, 1);
+  layout->addWidget(current_time_label_ptr_, 1, 0);
+  layout->addWidget(current_time_value_ptr_, 1, 1);
+  layout->addWidget(end_time_label_ptr_, 2, 0);
+  layout->addWidget(end_time_value_ptr_, 2, 1);
+  layout->addWidget(replay_speed_label_ptr_, 3, 0);
+  layout->addWidget(replay_speed_input_ptr_, 3, 1);
+  layout->addWidget(step_size_label_ptr_, 4, 0);
+  layout->addWidget(step_size_input_ptr_, 4, 1);
+  layout->addWidget(play_button_ptr_, 5, 0);
+  layout->addWidget(step_button_ptr_, 5, 1);
+  layout->addWidget(pause_button_ptr_, 6, 0, 1, 2);
 
   setLayout(layout);
 
@@ -79,12 +63,30 @@ ReplayPanel::ReplayPanel(QWidget * parent) : rviz_common::Panel(parent)
   executor_thread_ = std::thread([this]() { executor_.spin(); });
 }
 
-void ReplayPanel::state_callback(const ReplayerStateMsg::ConstSharedPtr state)
+template <typename T>
+inline void set_timestamp_as_text(QLabel & label, const T & timestamp_msg)
 {
-  RCLCPP_INFO(node_ptr_->get_logger(), "HOI");
+  label.setText(QString::number(
+    rclcpp::Time(timestamp_msg).seconds(), ReplayPanel::kValueDisplayFormat,
+    ReplayPanel::kValueDisplayPrecision));
 }
 
-void ReplayPanel::onInitialize() {}
+void ReplayPanel::state_callback(const ReplayerStateMsg::ConstSharedPtr state_ptr)
+{
+  set_timestamp_as_text(*start_time_value_ptr_, state_ptr->start_time);
+  set_timestamp_as_text(*current_time_value_ptr_, state_ptr->current_time);
+  set_timestamp_as_text(*end_time_value_ptr_, state_ptr->end_time);
+
+  if (state_ptr->is_playing) {
+    play_button_ptr_->setDisabled(true);
+    pause_button_ptr_->setDisabled(false);
+    step_button_ptr_->setDisabled(true);
+  } else {
+    play_button_ptr_->setDisabled(false);
+    pause_button_ptr_->setDisabled(true);
+    step_button_ptr_->setDisabled(false);
+  }
+}
 
 ReplayPanel::~ReplayPanel()
 {
