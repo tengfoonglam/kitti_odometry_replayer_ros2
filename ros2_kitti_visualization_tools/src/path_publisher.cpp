4552bb9ad4887a3d049253e38d56e64eeb515bf6
Lam Teng Foong <tengfoonglam@yahoo.com.sg>
2023-08-21 23:53:26 +0200

    Cleanup

--- ros2_kitti_visualization_tools/src/path_publisher.cpp
+++ ros2_kitti_visualization_tools/src/path_publisher.cpp
@@ -40,36 +40,33 @@ void PathPublisher::publish_line(const geometry_msgs::msg::TransformStamped & tr
 {
   std::scoped_lock lock(mutex_);
 
-  if (!previous_transform_stamped_.has_value()) {
-    previous_transform_stamped_ = transform_stamped;
-    return;
+  if (previous_transform_stamped_.has_value()) {
+    auto msg_ptr = std::make_unique<Marker>();
+    msg_ptr->header = transform_stamped.header;
+    msg_ptr->ns = "line";
+    msg_ptr->id = line_id_++;
+    msg_ptr->type = Marker::LINE_STRIP;
+    msg_ptr->action = Marker::ADD;
+    msg_ptr->scale.x = 0.1;
+    msg_ptr->color.r = 1.0;
+    msg_ptr->color.a = 1.0;
+
+    using Point = Marker::_points_type::value_type;
+
+    Point previous_point;
+    previous_point.x = previous_transform_stamped_.value().transform.translation.x;
+    previous_point.y = previous_transform_stamped_.value().transform.translation.y;
+    previous_point.z = previous_transform_stamped_.value().transform.translation.z;
+    msg_ptr->points.push_back(previous_point);
+
+    Point current_point;
+    current_point.x = transform_stamped.transform.translation.x;
+    current_point.y = transform_stamped.transform.translation.y;
+    current_point.z = transform_stamped.transform.translation.z;
+    msg_ptr->points.push_back(previous_point);
+
+    publisher_ptr_->publish(std::move(msg_ptr));
   }
-
-  auto msg_ptr = std::make_unique<Marker>();
-  msg_ptr->header = transform_stamped.header;
-  msg_ptr->ns = "line";
-  msg_ptr->id = line_id_++;
-  msg_ptr->type = Marker::LINE_STRIP;
-  msg_ptr->action = Marker::ADD;
-  msg_ptr->scale.x = 0.1;
-  msg_ptr->color.r = 1.0;
-  msg_ptr->color.a = 1.0;
-
-  using Point = Marker::_points_type::value_type;
-
-  Point previous_point;
-  previous_point.x = previous_transform_stamped_.value().transform.translation.x;
-  previous_point.y = previous_transform_stamped_.value().transform.translation.y;
-  previous_point.z = previous_transform_stamped_.value().transform.translation.z;
-  msg_ptr->points.push_back(previous_point);
-
-  Point current_point;
-  current_point.x = transform_stamped.transform.translation.x;
-  current_point.y = transform_stamped.transform.translation.y;
-  current_point.z = transform_stamped.transform.translation.z;
-  msg_ptr->points.push_back(previous_point);
-
-  publisher_ptr_->publish(std::move(msg_ptr));
 
   previous_transform_stamped_ = transform_stamped;
 }
